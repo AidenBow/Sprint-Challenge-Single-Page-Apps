dda9f072eaf0bf729aba6694e16f4af7b8eb8fa8
AidenBow <AidenBow@users.noreply.github.com>
2019-08-16 19:43:10 -0700

    added router and navlink navigation

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,7 +2,21 @@ import React from "react";
 import { Tab, Menu, Icon } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 
-// TODO: Add missing tabs below
 export default function TabNav() {
-
+    return(
+        <Menu>
+            <Menu.Item>
+                <NavLink exact to="/"> Home </NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to="/characters"> Characters </NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to="/episodes"> Episodes </NavLink>
+            </Menu.Item>
+            <Menu.Item>
+                <NavLink to="/locations"> Locations </NavLink>
+            </Menu.Item>
+        </Menu>
+    )
 };
